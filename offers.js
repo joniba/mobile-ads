db5d37fe5d78d6f8d0d4baf25079c155d68d525f
Ben Ami, Jonathan <jb250375@ncr.com>
2015-06-24 16:48:56 +0300

    bit of cleanup

--- offers.js
+++ offers.js
@@ -2,23 +2,29 @@
 var rest = require('restling');
 var Promise = require('bluebird');
 
-module.exports.getOffers = function (advertiser) {
-  var url = buildUrl(advertiser.ca_api_url, advertiser.ca_api_id, advertiser.ca_api_token, 1);
+module.exports = function () {
+  
+  var self = {
 
-  return rest.get(url).then(function (res) {
-    return new Promise(function (resolve, reject) {
-      resolve(_.reduce(res.data.data.data, function (result, val, key) {
-        result.push({
-          io_id: key,
-          io_app_name: /.+?(?=-|$)/.exec(val.name)[0].trim(),
+    getOffers: function (advertiser) {
+      var url = buildUrl(advertiser.ca_api_url, advertiser.ca_api_id, advertiser.ca_api_token, 1);
+      
+      return rest.get(url).then(function (res) {
+        return new Promise(function (resolve, reject) {
+          resolve(_.map(res.data.data.data, function (val, key) {
+            return {
+              io_id: key,
+              io_app_name: /.+?(?=-|$)/.exec(val.name)[0].trim(),
+            };
+          }));
         });
-        
-        return result;
-      }, []));
-    });
-  });
+      });
+    }
+  };
+  
+  function buildUrl(url, apiId, token, page) {
+    return url + '?api_id=' + apiId + '&api_token=' + token + '&limit=10&page=' + page;
+  }
+  
+  return self;
 };
-
-function buildUrl(url, apiId, token, page) {
-  return url + '?api_id=' + apiId + '&api_token=' + token + '&limit=10&page=' + page;
-}
--- test/offers.spec.js
+++ test/offers.spec.js
@@ -1,17 +1,27 @@
-﻿require('chai').should();
-var offers = require('../offers.js');
+﻿var chai = require('chai');
+var offers = require('../offers.js')();
 var settings = require('../config.js')();
+var chaiAsPromised = require("chai-as-promised");
+
+before(function () {
+  chai.should();
+  chai.use(chaiAsPromised);
+});
 
 describe('offers', function () {
   describe('#getOffers', function () {
     it('should return offers model', function (done) {
       this.timeout(8000);
+      
       offers.getOffers({
         ca_api_url: 'http://sync.yeahmobi.com/sync/offer/get',
         ca_api_id: settings.apiId,
         ca_api_token: settings.apiToken
       }).done(function (result) {
         result.should.have.length(10);
+        result[0].should.have.property('io_id');
+        result[0].should.have.property('io_app_name');
+
         done();
       });
     });
